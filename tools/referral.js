84359ee3af1e03dc4bca166a0ac6888ff2da3144
Zergity <zergity@gmail.com>
2020-07-16 18:54:51 +0700

    finalize the commission formula for referral script

--- tools/referral.js
+++ tools/referral.js
@@ -1,6 +1,6 @@
 
-const LN_2 = Math.log(2);
-console.log(LN_2, 1/LN_2);
+// const LN_2 = Math.log(2);
+// console.log(LN_2, 1/LN_2);
 
 const outstandingRent = 13*12;
 const totalRent = 13 + outstandingRent;
@@ -8,44 +8,42 @@ const T = Math.sqrt(totalRent);
 // const T = totalRent;
 console.log(`T: ${T}`);
 const levelCount = 8;
-const levelStep = T / levelCount;
-console.log(`level step: ${levelStep}`);
+const S = T / levelCount;
+console.log(`level step: ${S}`);
 
 const rents = [
-  [12,1],
-  [6,6,1],
-  [4,4,4,1],
-  [3,3,3,3,1],
-  [2,2,2,2,2,2,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1],
+  [12],
+  [6,6],
+  [4,4,4],
+  [3,3,3,3],
+  [12,0,0,0],
+  [0,0,0,12],
+  [2,2,2,2,2,2],
+  [1,1,1,1,1,1,1,1,1,1,1,1],
 ];
+
+function nextRemain(remain, r) {
+  return remain / Math.pow(2,r/S);
+}
+
+function nextReward(remain, r) {
+  return remain - nextRemain(remain, r);
+}
 
 for (const rent of rents) {
   console.log(`\n${rent}`);
   let remain = 32;
   for (let r of rent) {
-    console.log(`r: ${r.toFixed(2)}`);
-    const a = r/levelStep;
-    const y0 = remain * LN_2;
+    // console.log(`r: ${r.toFixed(2)}`);
+    // const y0 = remain * LN_2;
     // remain is the area under y = y0 / 2^x
     // y0 = remain * ln(2)
     // a = r/step
     // reward = y0 * integral(1/2^x) for x = [0,a]
     // reward = y0 * (1-1/2^a) / ln(2) = remain * (1-1/2^a)
-    const reward = remain*(1-1/Math.pow(2,a));
+    const reward = nextReward(remain, r);
     remain -= reward;
-
-    // let reward = 0;
-    // while (r >= levelStep) {
-    //   r -= levelStep;
-    //   remain /= 2;
-    //   reward += remain;
-    // }
-    // if (r > 0) {
-    //   const partial = (r / levelStep) * remain / 2;
-    //   reward += partial
-    //   remain -= partial;
-    // }
-    console.log(`reward: ${reward.toFixed(2)}, remain: ${remain.toFixed(2)}`);
+    // console.log(`reward: ${reward.toFixed(2)}, remain: ${remain.toFixed(2)}`);
   }
+  console.log(`remain: ${remain.toFixed(2)}`)
 }
